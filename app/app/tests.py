33196571e88f617ec44cc643ee7d01519f4e0e8c
Prakash1099 <prakashukkaravel@gmail.com>
2026-09-18 02:23:37 +0530

    Fixed some more linting issues

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,10 +11,8 @@ class CalcTests(SimpleTestCase):
         """test adding nums togather"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-        
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numebr"""
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
-        
